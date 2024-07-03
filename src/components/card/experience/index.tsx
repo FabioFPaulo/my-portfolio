cdc530c2bcafcd9302d874b48398a6bdb5f0874a
Fábio Fraga Paulo <fabiofrapaulo@outlook.pt>
2024-07-03 13:55:39 +0100

    update experiences animations

--- src/components/card/experience/index.tsx
+++ src/components/card/experience/index.tsx
@@ -1,36 +1,21 @@
 import "./index.scss";
-import useApp from "@/hooks/useApp";
-import { useEffect, useRef } from "react";
-import { useAnimation, useInView, motion } from "framer-motion";
+import { motion } from "framer-motion";
 import { CardExperienceProps } from "./types";
 
+const variants = {
+  open: {
+    opacity: 1,
+    y: 0,
+  },
+  closed: {
+    opacity: 0,
+    y: 74,
+  },
+};
+
 export default function CardExperienceComponent(props: CardExperienceProps) {
-  const app = useApp();
-
-  const ref = useRef(null);
-  const isInView = useInView(ref, { once: false });
-
-  const mainControls = useAnimation();
-
-  useEffect(() => {
-    if (isInView && !app.isLoading) {
-      mainControls.start("visible");
-    } else {
-      mainControls.set("hidden");
-    }
-  }, [isInView, mainControls, app.isLoading]);
   return (
-    <motion.div
-      ref={ref}
-      className="card-experience"
-      variants={{
-        hidden: { opacity: 0, y: 74 },
-        visible: { opacity: 1, y: 0 },
-      }}
-      initial="hidden"
-      animate={mainControls}
-      transition={{ duration: 1.25, delay: 0.25 }}
-    >
+    <motion.div className="card-experience" variants={variants}>
       <div className="date">{props.date}</div>
       <div className="content">
         <div className="item-title">{props.title}</div>
--- src/sections/experiences/index.tsx
+++ src/sections/experiences/index.tsx
@@ -1,17 +1,50 @@
+import useApp from "@/hooks/useApp";
 import Cards from "../../components/card";
 import Container from "../../components/container";
 import Title from "../../components/title";
 import "./index.scss";
 import { experiencesData, formatStartEndDate } from "./utils";
+import { useEffect, useRef } from "react";
+import { useAnimation, useInView, motion } from "framer-motion";
+
+const variants = {
+  open: {
+    transition: { staggerChildren: 0.1, delayChildren: 0.5 },
+  },
+  closed: {
+    transition: { staggerChildren: 0.1, staggerDirection: -1 },
+  },
+};
 
 export default function Experiences() {
+  const app = useApp();
+
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: false });
+
+  const mainControls = useAnimation();
+
+  useEffect(() => {
+    if (isInView && !app.isLoading) {
+      mainControls.start("open");
+    } else {
+      mainControls.start("closed");
+    }
+  }, [isInView, mainControls, app.isLoading]);
+
   return (
     <Container.Section colorScheme="color-1" id="experiences">
       <Title.Primary
         title="Experiences"
         subtitle="Discover the experiences that have shaped my professional journey"
       />
-      <div className="list">
+      <motion.div
+        ref={ref}
+        variants={variants}
+        initial="closed"
+        animate={mainControls}
+        className="list"
+      >
         {experiencesData.map((item, index) => (
           <Cards.Experience
             key={index}
@@ -21,7 +54,7 @@ export default function Experiences() {
             description={item.description}
           />
         ))}
-      </div>
+      </motion.div>
     </Container.Section>
   );
 }
